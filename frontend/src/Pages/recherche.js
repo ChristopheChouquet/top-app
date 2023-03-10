7e86323dbe7d01cd34c3f31f4af1c7e6ec82399f
ChristopheChouquet <chouquet.christophee@gmail.com>
2023-03-10 10:25:40 +0100

    Ajout du componenent FormRecherche

--- frontend/src/Pages/recherche.js
+++ frontend/src/Pages/recherche.js
@@ -2,17 +2,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Footer from "../Components/Footer";
+import FormRecherche from "../Components/FormRecherche";
 import Header from "../Components/Header";
 
 function Recherche() {
     // Stockage des tops
         const [user, setUser] = useState([]); 
-    //Mise en place de la gestion du form avec useForm
-        const {register, handleSubmit, reset} = useForm();
-
-        const onSubmit = function (data) {
-            console.log(data);
-        }
 
 
     useEffect(() => {
@@ -37,38 +32,18 @@ function Recherche() {
     }, []);
 
 
+    function dataRecherche(dataRecherche) {
+        console.log(dataRecherche);
+        
+    }
+
+
     return(
         <>
             <Header/>
             <div id="recherche" className="mb-16 mt-16">
                 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className='mt-5 flex flex-col px-20'>
-                        <div className="relative mt-2 rounded-md z-[-1]">
-                            <input 
-                                {...register("recherche")} 
-                                type="text"
-                                name="recherche" 
-                                id="recherche"
-                                placeholder="Recherche"
-                                autoComplete='off' 
-                                className="w-full text-tertiary-400 border-2 border-primary rounded-xl placeholder:text-sm placeholder:text-tertiary-300 pl-2 focus:outline-none"
-                                
-                            />
-                            <div className="absolute inset-y-0 right-0 flex items-center">
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    fill="none" viewBox="0 0 24 24" 
-                                    strokeWidth={1.5} 
-                                    className="w-5 h-5 stroke-tertiary-300 mr-2"
-                                    onClick={() => {reset()}}
-                                >
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                <FormRecherche dataRecherche={dataRecherche}/>
 
                 {user.map(user => (
                     <div key={user._id}>
